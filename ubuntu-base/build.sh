a8fe0c6cde4b37ae5cebca29b2a6d6d5cbcc9c98
mrkuz <markus@bitsandbobs.net>
2020-04-11 15:42:31 +0200

    Add ansbile and nvidia driver to base image

--- ubuntu-base/build.sh
+++ ubuntu-base/build.sh
@@ -6,10 +6,12 @@ if [[ "$#" -ne 1 ]]; then
 fi
 
 # docker build --no-cache \
+NVIDIA_VERSION="$(nvidia-smi | sed -n 's/.*Driver Version: //;T;s/\..*//;p')"
 docker build \
     --build-arg USER="$(id -un)" \
     --build-arg USER_ID="$(id -u)" \
     --build-arg GROUP="$(id -gn)" \
     --build-arg GROUP_ID="$(id -g)" \
+    --build-arg NVIDIA_VERSION="$NVIDIA_VERSION" \
     -t "$1" \
     .
