ed9fac56b8dbbb8ddbdead75b5a8677c0093b0d6
mrkuz <markus@bitsandbobs.net>
2020-04-24 20:31:33 +0200

    Introduce ubuntu-base-cli

--- ubuntu-base-x11d/build.sh
+++ ubuntu-base-x11d/build.sh
@@ -5,13 +5,8 @@ if [[ "$#" -ne 1 ]]; then
     exit 1
 fi
 
-# docker build --no-cache \
 NVIDIA_VERSION="$(nvidia-smi | sed -n 's/.*Driver Version: //;T;s/\..*//;p')"
 docker build \
-    --build-arg USER="$(id -un)" \
-    --build-arg USER_ID="$(id -u)" \
-    --build-arg GROUP="$(id -gn)" \
-    --build-arg GROUP_ID="$(id -g)" \
     --build-arg NVIDIA_VERSION="$NVIDIA_VERSION" \
     -t "$1" \
     .
