410bfdf85089bc13579fd4020dbd1742a0f4ec33
mrkuz <markus@bitsandbobs.net>
2020-09-05 08:03:44 +0200

    Add ubuntu-shell

---
+++ ubuntu-shell/build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+if [[ "$#" -ne 1 ]]; then
+    echo "Usage: $0 TAG"
+    exit 1
+fi
+
+docker build \
+    --build-arg USER="$(id -un)" \
+    --build-arg USER_ID="$(id -u)" \
+    --build-arg GROUP="$(id -gn)" \
+    --build-arg GROUP_ID="$(id -g)" \
+    -t "$1" \
+    .
